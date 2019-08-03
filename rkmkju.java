dee5894f3b1bf01d0656e5af84a58e2955589432
unknown <tanapat326@gmail.com>
2019-08-03 00:20:05 -0700

    variable java

--- rkmkju.java
+++ rkmkju.java
@@ -1,5 +1,18 @@
 public class rkmkju {
     public static void main(String[] args) {
-        System.out.println("rkmkju");
+        int number = 10;
+        long longNumber = 100;
+        double doubleNumber = 1.5; 
+        float floatNumber = 3.0f;
+
+        char chartext = 'c';
+        String text =  "message";
+        
+        System.out.println(number);
+        System.out.println(longNumber);
+        System.out.println(doubleNumber);
+        System.out.println(floatNumber);
+        System.out.println(chartext);
+        System.out.println(text);
     }
 }
